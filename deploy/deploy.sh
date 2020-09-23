0741d521a2a237510d3d62417728c9cefb977807
samir <rimas.amga@gmail.com>
2020-09-23 16:50:43 +0700

    add auto deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,30 @@
+echo "copy docker-compose file"
+scp ./docker-compose.yml $1@$2:/data/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME/docker-compose.yml;
+
+echo "copy docker-compose file"
+if [[ "$ENV" == "develop" ]]; then
+    echo "SENTRY_DSN=$SENTRY_DSN_DEVELOP" >> .env
+ scp $SSH_OPT ./docker-compose.stage.yml $1@$2:/data/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME/docker-compose.yaml;
+elif [[ "$ENV" == "master" ]]; then
+    echo "SENTRY_DSN=$SENTRY_DSN_MASTER" >> .env
+  scp $SSH_OPT ./docker-compose.master.yml $1@$2:/data/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME/docker-compose.yaml;
+fi
+
+echo "copy .env file"
+scp ./.env $1@$2:/data/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME/.env
+
+
+echo "pull images"
+ssh $1@$2 "cd /data/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME/ && docker-compose pull"
+
+
+echo "migrate..."
+ssh $1@$2 "cd /data/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME/ && docker-compose run --rm server bash -c \"python manage.py migrate --noinput\""
+
+
+echo "start services"
+ssh $1@$2 "cd /data/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME/ && docker-compose up -d"
+
+
+echo "remove none docker images"
+ssh $1@$2 'docker image prune -a -f'
