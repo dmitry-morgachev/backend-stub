031d80d8d35b3ac3a248dad6e3971cbbcc6ae6ec
samir <rimas.amga@gmail.com>
2020-09-28 11:48:04 +0700

    fix flake8 config

--- server/config/settings/settings.py
+++ server/config/settings/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import dj_database_url
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).parents[2]
 SECRET_KEY = '!6xmo&@!7dzw8p6yxjnj&&1lur%4+fs!r2tuzb#6j(64s@m6)*'
 ROOT_URLCONF = 'config.urls'
 WSGI_APPLICATION = 'config.wsgi.application'
@@ -32,8 +32,8 @@ LOGGING = {
     'disable_existing_loggers': False,
     'filters': {
         'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
     },
     'handlers': {
         'console': {
@@ -48,7 +48,7 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
-    }
+    },
 }
 
 ##################################################################
@@ -58,7 +58,7 @@ LOGGING = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR.joinpath('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,7 +109,7 @@ if not DEBUG:
 ##################################################################
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATICFILES_DIRS = ('static',)
 
 STATICFILES_FINDERS = (
@@ -118,7 +118,7 @@ STATICFILES_FINDERS = (
 )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 FILE_UPLOAD_PERMISSIONS = 0o777
 FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o777
@@ -128,14 +128,14 @@ FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o777
 ##################################################################
 
 if DEBUG:
-    from .installed_apps import *
+    from .installed_apps import INSTALLED_APPS
 
     def show_toolbar(request):
         from django.conf import settings
         return settings.DEBUG
 
     DEBUG_TOOLBAR_CONFIG = {
-        "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
     }
-    MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware', ] + MIDDLEWARE
-    INSTALLED_APPS += ['debug_toolbar',]
+    MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+    INSTALLED_APPS += ['debug_toolbar']
